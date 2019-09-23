fc13cb48ed470bd4467d765db42cd1c80d181473
Artyom2811 <Rfrdctckj;yj4252>
2019-09-23 22:13:21 +0300

    Добавлены комментарии к методам.

--- src/main/java/Elevator.java
+++ src/main/java/Elevator.java
@@ -9,6 +9,7 @@ public class Elevator {
     boolean stopButton = false;
     List<СommandForElevator> listPanelElevator = new LinkedList<>();
 
+    //Метод запуска который возвращает конечную высоту лифта и количество произведенных действий лифта
     public int[] start() {
         while (listPanelElevator.size() != 0) {
             execution();
@@ -16,11 +17,13 @@ public class Elevator {
         return new int[]{height, numberOfActions};
     }
 
+    //Метод выполнения комманд
     private void execution() {
         ArrayList<Integer> route = createRoute();
         move(route);
     }
 
+    //Метод составления маршрута
     private ArrayList<Integer> createRoute() {
         int startFloor = listPanelElevator.get(0).numberFloorPosition;
         int finishFloor = listPanelElevator.get(0).numberFloorRequired;
@@ -32,6 +35,7 @@ public class Elevator {
         return list;
     }
 
+    //Метод выполнения действий
     private void move(ArrayList<Integer> route) {
         for (Integer el : route) {
             System.err.println("Лифт едет на " + el + " этаж");
@@ -67,7 +71,7 @@ public class Elevator {
         }
     }
 
-
+    //Метод поиска попутчиков
     private void searchForTravelCompanions(int startFloor, int finishFloor, ArrayList<Integer> list) {
         //Если лифт едет вниз
         if (startFloor > finishFloor) {
@@ -89,6 +93,7 @@ public class Elevator {
 
     }
 
+    //Метод нажатия на кнопку Стоп
     private void pressStopButton() {
         if (stopButton) {
             stopButton = false;
